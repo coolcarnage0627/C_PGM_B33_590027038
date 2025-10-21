12d8799ad4fe77aa7aec17f4d8896c2a7d356973
coolcarnage0627 <shaswatswetans0627@gmail.com>
2025-10-21 16:53:23 +0530

    Add files via upload

---
+++ 1day.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+ 
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    
+    c = a + b;
+
+ 
+    printf("The sum of %d and %d is: %d\n", a, b, c);
+
+    return 0;
+}
